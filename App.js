9a08bb26afbcab1c31f8286857f61e61597cc7c4
Dan Kiss <flowfast47@gmail.com>
2018-06-01 16:58:25 -0700

    add more styling, using flex

--- App.js
+++ App.js
@@ -1,41 +1,89 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button, AppRegistry, Image } from 'react-native';
 
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {isShowingText: true};
-
-    //toggle the state every second
-    setInterval(() => {
-      this.setState(previousState => {
-        return { isShowingText: !previousState.isShowingText };
-      });
-    }, 1000);
-  }
-
-  render() {
-    let display = this.state.isShowingText ? this.props.text : ' ';
-    return (
-      <Text>
-        {display}
-      </Text>
-    );
-  }
-}
-
-export default class BlinkApp extends React.Component {
+export default class FixedDimensionsBasics extends Component {
   render() {
     return (
-      <View style={{alignItems: 'center', backgroundColor: 'yellow', marginTop: 50}}>
-        <Blink text='I love to blink2' />
-        <Blink text='Blinking is fun' />
-        <Blink text='Why not wink?' />
-        <Blink text='Look at me wheee' />
+      <View style={{flex: 1}}>
+        <View style={{ flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{ flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{ flex: 3, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
 }
+
+// export default class FixedDimensionsBasics extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+//         <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+//         <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+//       </View>
+//     );
+//   }
+// }
+
+// export default class LotsOfStyles extends React.Component {
+//   render() {
+//     return (
+//       <View>
+//         <Text style={styles.red}>just red</Text>
+//         <Text style={styles.bigblue}>just bigblue</Text>
+//         <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+//         <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   bigblue: {
+//     color: 'blue',
+//     fontWeight: 'bold',
+//     fontSize: 30,
+//   },
+//   red: {
+//     color: 'red',
+//   },
+// });
+
+// class Blink extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {isShowingText: true};
+//
+//     //toggle the state every second
+//     setInterval(() => {
+//       this.setState(previousState => {
+//         return { isShowingText: !previousState.isShowingText };
+//       });
+//     }, 1000);
+//   }
+//
+//   render() {
+//     let display = this.state.isShowingText ? this.props.text : ' ';
+//     return (
+//       <Text>
+//         {display}
+//       </Text>
+//     );
+//   }
+// }
+//
+// export default class BlinkApp extends React.Component {
+//   render() {
+//     return (
+//       <View style={{alignItems: 'center', backgroundColor: 'yellow', marginTop: 50}}>
+//         <Blink text='I love to blink' />
+//         <Blink text='Blinking is fun' />
+//         <Blink text='Why not wink?' />
+//         <Blink text='Look at me wheee' />
+//       </View>
+//     );
+//   }
+// }
 
 // class Greeting extends Component {
 //   render() {
