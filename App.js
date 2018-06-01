fa5e747145b90780494f8c1fc035259c9fec7582
Dan Kiss <flowfast47@gmail.com>
2018-05-31 18:08:12 -0700

    make greeting component and reuse it multiple times

--- App.js
+++ App.js
@@ -3,16 +3,38 @@ import { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { AppRegistry, Image } from 'react-native';
 
-export default class Bananas extends Component {
+class Greeting extends Component {
   render() {
-    let pic = {
-      uri: 'http://natureandnutrition.com/wp-content/uploads/2015/04/Health-Benefits-of-Bananas-600x600.jpg'
-    };
     return (
-      <Image source={pic} style={{width: 193, height: 150}}/>
+      <Text> Hello {this.props.name}!</Text>
     );
   }
 }
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Don' />
+        <Greeting name='Jizanthepus' />
+      </View>
+    );
+  }
+}
+
+
+
+// export default class Bananas extends Component {
+//   render() {
+//     let pic = {
+//       uri: 'http://natureandnutrition.com/wp-content/uploads/2015/04/Health-Benefits-of-Bananas-600x600.jpg'
+//     };
+//     return (
+//       <Image source={pic} style={{width: 193, height: 150}}/>
+//     );
+//   }
+// }
 
 // export default class App extends React.Component {
 //   render() {
