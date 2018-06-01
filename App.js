3c2b61ed3cc4d9df73286dba689b03b6621f1fcf
Dan Kiss <flowfast47@gmail.com>
2018-05-31 17:49:30 -0700

    setup basic project and pass in image property of banana

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { AppRegistry, Image } from 'react-native';
+
+export default class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'http://natureandnutrition.com/wp-content/uploads/2015/04/Health-Benefits-of-Bananas-600x600.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 150}}/>
+    );
+  }
+}
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Look I changed something! Open up App.js to start working on your app!</Text>
+//         <Button
+//           title="Click me"
+//         />
+//         <Text>Changes you make will automatically reload.</Text>
+//         <Text>Shake your phone to open the developer menu.</Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
