140cc0a5cdd6cd8dc0f095440aadc0ff84302296
Dan Kiss <flowfast47@gmail.com>
2018-06-03 16:59:20 -0700

    add touchable button

--- App.js
+++ App.js
@@ -1,35 +1,88 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button, AppRegistry, Image, TextInput, Alert } from 'react-native';
 
-
-
-export default class PizzaTranslator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button')
   }
-
   render() {
     return(
-      <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 40}}
-          placeholder='Type text here to translate to pizza'
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
-        <Button
-          onPress={() => {
-            Alert.alert('You tapped the button!');
-          }}
-          title='Press Me'
-        />
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title='Press me'
+          />
+        </View>
+        <View style={styles.buttonContainer} >
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color='#841584'
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="This looks great!"
+          />
+          <Button
+            onPress={this._onPressButton}
+            title="This looks great"
+          />
+          <Button
+            onPress={this._onPressButton}
+            title='Ok!'
+            color='#841584'
+          />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
+
+// export default class PizzaTranslator extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {text: ''};
+//   }
+//
+//   render() {
+//     return(
+//       <View style={{padding: 10}}>
+//         <TextInput
+//           style={{height: 40}}
+//           placeholder='Type text here to translate to pizza'
+//           onChangeText={(text) => this.setState({text})}
+//         />
+//         <Text style={{padding: 10, fontSize: 42}}>
+//           {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+//         </Text>
+//         <Button
+//           onPress={() => {
+//             Alert.alert('You tapped the button!');
+//           }}
+//           title='Press Me'
+//         />
+//       </View>
+//     );
+//   }
+// }
 
 // export default class JustifyContentBasics extends Component {
 //   render() {
