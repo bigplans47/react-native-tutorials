9872e37cbfc5b4ecb2739c7c6a08668b77a99971
Dan Kiss <flowfast47@gmail.com>
2018-06-03 20:50:16 -0700

    make a sectionlist

--- App.js
+++ App.js
@@ -1,55 +1,18 @@
 import React, { Component } from 'react';
-import { Alert, ScrollView, AppRegistry, Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View, Button, Image, TextInput, FlatList } from 'react-native';
+import { Alert, ScrollView, AppRegistry, Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View, Button, Image, TextInput, FlatList, SectionList } from 'react-native';
 
-export default class FlatListBasics extends Component {
+export default class SectionListBasics extends Component {
   render() {
-    return (
+    return(
       <View style={styles.container}>
-        <FlatList data={[
-          {key: 'Dan'},
-          {key: 'Ron'},
-          {key: 'Trixy'},
-          {key: 'Misty'},
-          {key: 'James'},
-          {key: 'Joel'},
-          {key: 'John'},
-          {key: 'Jillian'},
-          {key: 'Jimmy'},
-          {key: 'Julie'},
-          {key: 'Trixy'},
-          {key: 'Misty'},
-          {key: 'James'},
-          {key: 'Joel'},
-          {key: 'John'},
-          {key: 'Jillian'},
-          {key: 'Jimmy'},
-          {key: 'Julie'},
-          {key: 'Trixy'},
-          {key: 'Misty'},
-          {key: 'James'},
-          {key: 'Joel'},
-          {key: 'John'},
-          {key: 'Jillian'},
-          {key: 'Jimmy'},
-          {key: 'Julie'},
-          {key: 'Trixy'},
-          {key: 'Misty'},
-          {key: 'James'},
-          {key: 'Joel'},
-          {key: 'John'},
-          {key: 'Jillian'},
-          {key: 'Jimmy'},
-          {key: 'Julie'},
-          {key: 'Trixy'},
-          {key: 'Misty'},
-          {key: 'James'},
-          {key: 'Joel'},
-          {key: 'John'},
-          {key: 'Jillian'},
-          {key: 'Jimmy'},
-          {key: 'Julie'}
-        ]} renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-      />
+        <SectionList
+          sections={[
+            {title: 'D', data: ['Devin']},
+            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+        />
       </View>
     );
   }
@@ -60,6 +23,15 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 22
   },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
   item: {
     padding: 10,
     fontSize: 18,
@@ -67,7 +39,71 @@ const styles = StyleSheet.create({
   },
 })
 
-
+// export default class FlatListBasics extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <FlatList data={[
+//           {key: 'Dan'},
+//           {key: 'Ron'},
+//           {key: 'Trixy'},
+//           {key: 'Misty'},
+//           {key: 'James'},
+//           {key: 'Joel'},
+//           {key: 'John'},
+//           {key: 'Jillian'},
+//           {key: 'Jimmy'},
+//           {key: 'Julie'},
+//           {key: 'Trixy'},
+//           {key: 'Misty'},
+//           {key: 'James'},
+//           {key: 'Joel'},
+//           {key: 'John'},
+//           {key: 'Jillian'},
+//           {key: 'Jimmy'},
+//           {key: 'Julie'},
+//           {key: 'Trixy'},
+//           {key: 'Misty'},
+//           {key: 'James'},
+//           {key: 'Joel'},
+//           {key: 'John'},
+//           {key: 'Jillian'},
+//           {key: 'Jimmy'},
+//           {key: 'Julie'},
+//           {key: 'Trixy'},
+//           {key: 'Misty'},
+//           {key: 'James'},
+//           {key: 'Joel'},
+//           {key: 'John'},
+//           {key: 'Jillian'},
+//           {key: 'Jimmy'},
+//           {key: 'Julie'},
+//           {key: 'Trixy'},
+//           {key: 'Misty'},
+//           {key: 'James'},
+//           {key: 'Joel'},
+//           {key: 'John'},
+//           {key: 'Jillian'},
+//           {key: 'Jimmy'},
+//           {key: 'Julie'}
+//         ]} renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+//       />
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     paddingTop: 22
+//   },
+//   item: {
+//     padding: 10,
+//     fontSize: 18,
+//     height: 44,
+//   },
+// })
 
 // export default class IScrollerDownAndWhatHappenedNextShockedMe extends Component {
 //   render() {
